d49cfec71d8e9972cd749fced4f4fb314962fb79
Karina <268549883+karinaferreiraw@users.noreply.github.com>
2026-04-13 06:47:34 -0300

    KarinaWachi_Ag8_DS_I

---
+++ KarinaWachi_Ag8_DS_I.py
@@ -0,0 +1,23 @@
+#Bem-vindos ao TudoWeb 
+###Sua opinião é muito importante para nós! ✨  
+
+# Contadores
+qtde_excelente = 0
+qtde_ruim = 0
+
+for i in range(1, 51):
+    print("\nEntrevistado número:", i)
+    nome = input("Digite o seu nome: ")
+    idade = int(input("Digite a sua idade: "))
+    opiniao = int(input("Qual a sua opinião sobre nós? (Sendo: 1=EXCELENTE, 2=BOM, 3=RUIM): "))
+
+    # Estrutura de decisão
+    if opiniao == 1:
+        qtde_excelente += 1
+    elif opiniao == 3:
+        qtde_ruim += 1
+
+# Resultado final
+print("\nRESULTADO DA PESQUISA")
+print("Quantidade de avaliações EXCELENTES:", qtde_excelente, "🤩")
+print("Quantidade de avaliações RUINS:", qtde_ruim, "🙁")
---
+++ README.md
@@ -0,0 +1,6 @@
+Pesquisa de opinião da empresa TudoWeb. 
+
+Digite seu nome, sua idade e opinião sobre o atendimento prestado, sendo:
+1: EXCELENTE
+2: BOM
+3: RUIM
